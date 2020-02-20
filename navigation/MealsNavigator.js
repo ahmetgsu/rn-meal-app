465071cf46b5c64674bed02256addb00168a0d6d
ahmetgsu <ahmet.gsu@gmail.com>
2020-02-20 10:44:33 +0300

    custom font added to tab bar navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Platform } from 'react-native';
+import { Platform, Text } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
@@ -16,9 +16,18 @@ import FiltersScreen from '../screens/FiltersScreen';
 import Colors from '../constants/Colors';
 
 const defaultStackNavOptions = {
+  // this targets header background, not the text
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
   },
+  // this targets the text of the header
+  headerTitleStyle: {
+    fontFamily: 'roboto-slab',
+    fontSize: 22
+  },
+  // this targets the text appears next to back button (only IOS)
+  // Android doe not have back-text
+  headerBackTitleStyle: {},
   headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor
 };
 
@@ -60,7 +69,14 @@ const tabScreenConfig = {
           <Ionicons name='ios-restaurant' size={25} color={tabInfo.tintColor} />
         );
       },
-      tabBarColor: Colors.primaryColor
+      tabBarColor: Colors.primaryColor,
+      // this targets tab bar label styling (for android)
+      tabBarLabel:
+        Platform.OS === 'android' ? (
+          <Text style={{ fontFamily: 'roboto-slab' }}>Meals</Text>
+        ) : (
+          'Meals'
+        )
     }
   },
   Favorites: {
@@ -70,7 +86,14 @@ const tabScreenConfig = {
       tabBarIcon: tabInfo => {
         return <Ionicons name='ios-star' size={25} color={tabInfo.tintColor} />;
       },
-      tabBarColor: Colors.secondaryColor
+      tabBarColor: Colors.secondaryColor,
+      // this targets tab bar label styling (for android)
+      tabBarLabel:
+        Platform.OS === 'android' ? (
+          <Text style={{ fontFamily: 'roboto-slab' }}>Favorites</Text>
+        ) : (
+          'Favorites'
+        )
     }
   }
 };
@@ -88,20 +111,52 @@ const MealsFavTabNavigator =
         {
           tabBarOptions: {
             // activeTintColor defines the active tab text's color
-            activeTintColor: Colors.secondaryColor
+            activeTintColor: Colors.secondaryColor,
+            // this targets the bottom tab label (only IOS)
+            labelStyle: {
+              fontFamily: 'roboto-slab'
+            }
           }
         }
       );
 
-const FiltersNavigator = createStackNavigator({
-  Filters: FiltersScreen
-});
+const FiltersNavigator = createStackNavigator(
+  {
+    Filters: FiltersScreen
+  },
 
-const MainNavigator = createDrawerNavigator({
-  // This navigator does not automatically add hamburger menu to related screens
-  // We need to add it manually
-  MealsFavs: MealsFavTabNavigator,
-  Filters: FiltersNavigator
-});
+  {
+    // navigationOptions: {
+    //   drawerLabel: 'Meal Filter'
+    // },
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+);
+
+const MainNavigator = createDrawerNavigator(
+  {
+    // This navigator does not automatically add hamburger menu to related screens
+    // We need to add it manually
+    MealsFavs: {
+      screen: MealsFavTabNavigator,
+      navigationOptions: { drawerLabel: 'Meals' }
+    },
+    Filters: {
+      screen: FiltersNavigator,
+      navigationOptions: {
+        drawerLabel: 'Meal Filter'
+      }
+    }
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.secondaryColor,
+      labelStyle: {
+        fontFamily: 'roboto-slab',
+        fontSize: 16
+      }
+    }
+  }
+);
 
 export default createAppContainer(MainNavigator);
