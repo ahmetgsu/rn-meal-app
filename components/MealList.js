0ba6b611c2e4653737d6d919e22af47baa620811
ahmetgsu <ahmet.gsu@gmail.com>
2020-02-23 23:34:05 +0300

    redux data - navigation options

--- components/MealList.js
+++ components/MealList.js
@@ -3,46 +3,47 @@ import { View, FlatList, StyleSheet } from 'react-native';
 import MealItem from './MealItem';
 
 const MealList = props => {
-  const renderMealItem = itemData => {
-    // console.log('itemData.item: ', itemData.item);
-    return (
-      <MealItem
-        title={itemData.item.title}
-        image={itemData.item.imageUrl}
-        duration={itemData.item.duration}
-        complexity={itemData.item.complexity}
-        affordability={itemData.item.affordability}
-        onSelectMeal={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetail',
-            params: {
-              mealId: itemData.item.id
-            }
-          });
-        }}
-      />
-    );
-  };
+	const renderMealItem = itemData => {
+		// console.log('itemData.item: ', itemData.item);
+		return (
+			<MealItem
+				title={itemData.item.title}
+				image={itemData.item.imageUrl}
+				duration={itemData.item.duration}
+				complexity={itemData.item.complexity}
+				affordability={itemData.item.affordability}
+				onSelectMeal={() => {
+					props.navigation.navigate({
+						routeName: 'MealDetail',
+						params: {
+							mealId: itemData.item.id,
+							mealTitle: itemData.item.title
+						}
+					});
+				}}
+			/>
+		);
+	};
 
-  return (
-    <View style={styles.list}>
-      <FlatList
-        data={props.listData}
-        keyExtractor={(item, index) => item.id}
-        renderItem={renderMealItem}
-        style={{ width: '95%' }}
-      />
-    </View>
-  );
+	return (
+		<View style={styles.list}>
+			<FlatList
+				data={props.listData}
+				keyExtractor={(item, index) => item.id}
+				renderItem={renderMealItem}
+				style={{ width: '95%' }}
+			/>
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  list: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    marginVertical: 10
-  }
+	list: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		marginVertical: 10
+	}
 });
 
 export default MealList;
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -1,93 +1,94 @@
-import React from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Button,
-  ScrollView,
-  Image
-} from 'react-native';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import DefaultText from '../components/DefaultText';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 
 const ListItem = props => {
-  return (
-    <View style={styles.listItem}>
-      <DefaultText>{props.children}</DefaultText>
-    </View>
-  );
+	return (
+		<View style={styles.listItem}>
+			<DefaultText>{props.children}</DefaultText>
+		</View>
+	);
 };
 
 const MealDetailScreen = props => {
-  const mealId = props.navigation.getParam('mealId');
+	const mealId = props.navigation.getParam('mealId');
 
-  const selectedMeal = MEALS.find(item => item.id === mealId);
-  return (
-    <ScrollView>
-      <Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
-      <View style={styles.details}>
-        <DefaultText>{selectedMeal.duration}minutes</DefaultText>
-        <DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
-        <DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
-      </View>
-      <Text style={styles.title}>Ingredients</Text>
-      {selectedMeal.ingredients.map(item => (
-        <ListItem key={item}>{item}</ListItem>
-      ))}
-      <Text style={styles.title}>Steps</Text>
-      {selectedMeal.steps.map((elem, index) => (
-        <ListItem key={index}>
-          {index + 1} - {elem}
-        </ListItem>
-      ))}
-    </ScrollView>
-  );
+	const availableMeals = useSelector(state => state.meals.meals);
+
+	const selectedMeal = availableMeals.find(item => item.id === mealId);
+
+	// useEffect(() => {
+	// 	props.navigation.setParams({ mealTitle: selectedMeal.title });
+	// }, [selectedMeal]);
+
+	return (
+		<ScrollView>
+			<Image source={{ uri: selectedMeal.imageUrl }} style={styles.image} />
+			<View style={styles.details}>
+				<DefaultText>{selectedMeal.duration}minutes</DefaultText>
+				<DefaultText>{selectedMeal.complexity.toUpperCase()}</DefaultText>
+				<DefaultText>{selectedMeal.affordability.toUpperCase()}</DefaultText>
+			</View>
+			<Text style={styles.title}>Ingredients</Text>
+			{selectedMeal.ingredients.map(item => (
+				<ListItem key={item}>{item}</ListItem>
+			))}
+			<Text style={styles.title}>Steps</Text>
+			{selectedMeal.steps.map((elem, index) => (
+				<ListItem key={index}>
+					{index + 1} - {elem}
+				</ListItem>
+			))}
+		</ScrollView>
+	);
 };
 
 MealDetailScreen.navigationOptions = navigationData => {
-  const mealId = navigationData.navigation.getParam('mealId');
-  const selectedMeal = MEALS.find(item => item.id === mealId);
-  return {
-    headerTitle: selectedMeal.title,
-    headerRight: () => (
-      <HeaderButtons HeaderButtonComponent={HeaderButton}>
-        <Item
-          title='Favorite'
-          iconName='ios-star'
-          onPress={() => {
-            console.log('Marked as favorite');
-          }}
-        />
-      </HeaderButtons>
-    )
-  };
+	const mealId = navigationData.navigation.getParam('mealId');
+	const mealTitle = navigationData.navigation.getParam('mealTitle');
+	// const selectedMeal = MEALS.find(item => item.id === mealId);
+	return {
+		headerTitle: mealTitle,
+		headerRight: () => (
+			<HeaderButtons HeaderButtonComponent={HeaderButton}>
+				<Item
+					title='Favorite'
+					iconName='ios-star'
+					onPress={() => {
+						console.log('Marked as favorite');
+					}}
+				/>
+			</HeaderButtons>
+		)
+	};
 };
 
 const styles = StyleSheet.create({
-  image: {
-    width: '100%',
-    height: 200
-  },
-  details: {
-    flexDirection: 'row',
-    padding: 15,
-    justifyContent: 'space-around'
-  },
-  title: {
-    fontFamily: 'roboto-slab',
-    fontSize: 22,
-    textAlign: 'center'
-  },
-  listItem: {
-    marginVertical: 10,
-    marginHorizontal: 20,
-    borderColor: '#ccc',
-    borderWidth: 1,
-    padding: 10
-  }
+	image: {
+		width: '100%',
+		height: 200
+	},
+	details: {
+		flexDirection: 'row',
+		padding: 15,
+		justifyContent: 'space-around'
+	},
+	title: {
+		fontFamily: 'roboto-slab',
+		fontSize: 22,
+		textAlign: 'center'
+	},
+	listItem: {
+		marginVertical: 10,
+		marginHorizontal: 20,
+		borderColor: '#ccc',
+		borderWidth: 1,
+		padding: 10
+	}
 });
 
 export default MealDetailScreen;
